2eb53ed8e3c02988a649b05dd46c5c844e132f60
astralaria <pawut.j@gmail.com>
2023-01-03 14:25:16 +0700

    Add Tes1,2,3,4

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func swapPointer(a, b *int) {
 	*b = temp
 }
 
-func main() {
+func test1() {
 	a := 1
 	b := 2
 
@@ -22,5 +22,69 @@ func main() {
 	fmt.Printf("a = %d,b = %d\n", a, b)
 	swapPointer(&a, &b)
 	fmt.Printf("a = %d,b = %d\n", a, b)
+}
 
+type SomeStructLevel2 struct {
+	value int
+}
+type SomeStruct struct {
+	value  int
+	level2 SomeStructLevel2
+}
+
+func ChangeStructValue(s SomeStruct) {
+	s.value = 2
+}
+
+func ChangeStructValuePointer(s *SomeStruct) {
+	s.value = 3
+}
+
+func test2() {
+	a := SomeStruct{value: 1}
+
+	ChangeStructValue(a)
+	fmt.Printf("a value : %d \n", a.value)
+	ChangeStructValuePointer(&a)
+	fmt.Printf("a value : %d \n", a.value)
+}
+
+func (s SomeStruct) ChangeStructValue() {
+	s.value = 4
+}
+
+func (s *SomeStruct) ChangeStructValuePointer() {
+	s.value = 5
+}
+func test3() {
+	a := SomeStruct{value: 1}
+	a.ChangeStructValue()
+	fmt.Printf("a value : %d \n", a.value)
+	a.ChangeStructValuePointer()
+	fmt.Printf("a value : %d \n", a.value)
+
+}
+
+func test4() {
+	a := SomeStruct{value: 1}
+	b := a
+
+	c := &a
+
+	a.ChangeStructValuePointer()
+	fmt.Printf("a value %d b value %d\n", a.value, b.value)
+	fmt.Printf("a value %d c value %d\n", a.value, c.value)
+
+	//	a.level2 := SomeStructLevel2{value : 10}
+	a.level2 = SomeStructLevel2{value: 10}
+	fmt.Printf("a value %d b value %d\n", a.level2.value, b.level2.value)
+	fmt.Printf("a value %d c value %d\n", a.level2.value, c.level2.value)
+
+}
+
+func main() {
+	test1()
+	test2()
+	test3()
+	test4()
 }
